184f2d1e009ae3c40647f6a76b2c354ae24c8c99
s21507BBacon <s215076784@deakin.edu.au>
2024-08-23 16:24:30 +1000

    orignal resources

--- app/config.py
+++ app/config.py
@@ -1,9 +1,6 @@
 import os
-from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
+class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 settings = Settings()
