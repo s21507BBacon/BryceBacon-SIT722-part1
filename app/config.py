f4aacee532bca88eba6ebb29bbf59bb457dee378
s21507BBacon <s215076784@deakin.edu.au>
2024-08-23 16:12:40 +1000

    updated files

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
+from pydantic_settings import BaseSettings
 
-class Settings:
+class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 settings = Settings()
--- app/db.py
+++ app/db.py
@@ -2,6 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config import settings
 
+print(f"DATABASE_URL: {settings.DATABASE_URL}")  # For debugging
+
 # SQLAlchemy database engine and session
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
